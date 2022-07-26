a5a1fa369459d8911f72400ffacf853b3619ee23
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-26 14:13:17 +0100

    Change code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,4 +21,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imageContainer = document.querySelector('.gallery');
+
+const imagesMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+imageContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+imageContainer.addEventListener('click', onImageContainerClick);
+
+function onImageContainerClick(e) {
+  e.preventDefault();
+
+  
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  let instance = '';
+  const getUrlbyDataSet = e.target.dataset.source;
+
+  const options = {
+    once: true,
+    // open Modal
+    onShow: (instance) => {
+      window.addEventListener('keydown', eventHandler);
+    },
+    // cloze Modal
+    onClose: (instance) => {
+      window.removeEventListener('keydown', eventHandler);
+    },
+  };
+  function eventHandler(e) {
+    if (e.key === 'Escape') {
+      instance.close();
+      return;
+    }
+  }
+  instance = basicLightbox.create(
+    `<img src="${getUrlbyDataSet}" width="800" height="600">`,
+    options
+  );
+  instance.show();
+}
