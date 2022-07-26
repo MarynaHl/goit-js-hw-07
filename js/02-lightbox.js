dfc315bf1fc08e98542cb07fbc2f484280286139
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-26 14:18:06 +0100

    Change code

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageContainer = document.querySelector(".gallery");
+const imagesMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+  })
+  .join("");
+
+imageContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+console.log(imageContainer);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  enableKeyboard: true,
+  animationSlide: true,
+  animationSpeed: 250,
+});
+
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
